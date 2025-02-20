1151ad7ddb10ec75a84578a687f8cd312e48ff62
jh <873750974@qq.com>
2025-02-20 17:10:36 +0800

    重构测试 1.9.1

--- openai-code-review-sdk/src/main/java/plus/gaga/middleware/domain/Model.java
+++ openai-code-review-sdk/src/main/java/plus/gaga/middleware/domain/Model.java
@@ -13,7 +13,7 @@ public enum Model {
     @Deprecated
     CHATGLM_PRO("chatglm_pro", "适用于对知识量、推理能力、创造力要求较高的场景"),
     /** 智谱AI 23年06月发布 */
-    CHATGLM_TURBO("chatglm_turbo", "适用于对知识量、推理能力、创造力要求较高的场景"),
+    CHATGLM_TURBO("chatglm_turbo", "适用于对知识量、推理能力、创造力要求较高的场景 "),
     /** 智谱AI 24年01月发布 */
     GLM_3_5_TURBO("glm-3-turbo","适用于对知识量、推理能力、创造力要求较高的场景"),
     GLM_4("glm-4","适用于复杂的对话交互和深度内容创作设计的场景"),
